051ecd075d7cd255fbc15d5ad15e762a9fdf6ad1
Serene Lee <serene2026@qq.com>
2025-11-12 22:06:26 +0800

    itheima.java.176

---
+++ Java/src/com/serene/practice1/Practice5.java
@@ -0,0 +1,18 @@
+package com.serene.practice1;
+
+/**
+ * Description:
+ *
+ * @author Serene Lee
+ * @date 2025/11/12
+ */
+public class Practice5 {
+    public static void main(String[] args) {
+        /*
+        判断任意的一个年份是闰年还是平年要求:用JDK7和JDK8两种方式判断提示:
+        二月有29天是闰年
+        一年有366天是闰年
+         */
+    }
+
+}
