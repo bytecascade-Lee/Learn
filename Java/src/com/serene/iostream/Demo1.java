33e14df53fd5b84bd93433658621118cbbcc09ec
Serene Lee <serene2026@qq.com>
2025-12-19 09:37:12 +0800

    itheima.java.check

---
+++ Java/src/com/serene/iostream/Demo1.java
@@ -0,0 +1,14 @@
+package com.serene.iostream;
+
+/**
+ * Description:
+ *
+ * @author Serene Lee
+ * @date 2025/12/19
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+
+    }
+
+}
