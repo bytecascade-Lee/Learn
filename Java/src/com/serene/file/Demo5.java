91a20a07fe7491b2c3b7192ec918e41972b3231a
Serene Lee <serene2026@qq.com>
2025-12-21 10:38:03 +0800

    itheima.java.modify

--- Java/src/com/serene/file/Demo5.java
+++ Java/src/com/serene/file/Demo5.java
@@ -1,4 +1,4 @@
-package com.serene.filedemo;
+package com.serene.file;
 
 import java.io.File;
 // import java.io.FilenameFilter;
---
+++ Java/src/com/serene/iostreampractice/Practice.java
@@ -0,0 +1,20 @@
+package com.serene.iostreampractice;
+
+/**
+ * Description:
+ *
+ * @author Serene Lee
+ * @date 2025/12/21
+ */
+public class Practice {
+    public static void main(String[] args) {
+        /*
+        修改文件中的数据
+        文本文件中有以下的数据：
+        2-1-9-4-7-8
+        将文件中的数据进行排序，变成以下的数据：
+        1-2-4-7-8-9
+         */
+    }
+
+}
